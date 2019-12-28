497efce1266b6235babe7f4ef6890b52b825362b
Gustav Utke Kauman <gustav.kauman@gmail.com>
2019-12-28 22:10:36 +0100

    Update expense migration

--- database/migrations/2019_12_28_203836_create_expenses_table.php
+++ database/migrations/2019_12_28_203836_create_expenses_table.php
@@ -15,6 +15,10 @@ class CreateExpensesTable extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('activity');
+            $table->double('amount');
+            $table->string('creditor');
+            $table->boolean('uploaded')->default(false);
             $table->timestamps();
         });
     }
