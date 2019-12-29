93fe4d8e0634d6a4790da0ec5d47537979347596
Gustav Utke Kauman <gustav.kauman@gmail.com>
2019-12-29 11:52:30 +0100

    Add styling to create expense page

--- resources/views/expense/create.blade.php
+++ resources/views/expense/create.blade.php
@@ -14,23 +14,24 @@
 
             <form action="/expense/store" method="post" enctype="multipart/form-data">
                 @csrf
-                <div class="text-2xl mt-6 text-left w-10/12 mx-auto">
+                <div class="text-2xl mt-6 text-left w-10/12 mx-auto bg-white p-4 px-8 rounded-lg">
+
                     <div class="my-8 py-4">
                         <label for="activity">Aktivitet</label>
-                        <input class="w-full" type="text" name="activity">
+                        <input class="w-full border-solid border-gray-400 border-2 rounded-lg" type="text" name="activity">
                     </div>
 
                     <div class="my-8 py-4">
                         <label for="amount">Beløb</label>
-                        <input class="w-full" type="number" step="any" name="amount">
+                        <input class="w-full border-solid border-gray-400 border-2 rounded-lg" type="number" step="any" name="amount">
                     </div>
 
                     <div class="my-8 py-4">
-                        <input class="block mx-auto" type="file" name="file">
+                        <input class="block mx-auto border-solid border-gray-400 border-2 rounded-lg" type="file" name="file">
                     </div>
 
                     <div class="my-8 py-4">
-                        <input type="submit" name="submit" value="Upload!">
+                        <input class="block mx-auto px-12 py-4 bg-green-500 text-white rounded-lg cursor-pointer" type="submit" name="submit" value="Upload!">
                     </div>
 
                 </div>
