66d340323734c665a9129aa2bf74910694999b07
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-01-05 20:07:47 +0100

    Add casting to date on Drive model

--- app/Drive.php
+++ app/Drive.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Drive extends Model
 {
 
+    // Attributes to be casted to a carbon instace
+    protected $dates = [
+        'date'
+    ];
+
+    // Get the associated user
     public function user()
     {
         return $this->belongsTo(User::class);
