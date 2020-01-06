acfc4ed42db775f99d4d233925c44f51d16203dd
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-01-06 21:27:32 +0100

    Add attributes needed for user drive log

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('address')->nullable();
+            $table->string('license_plate')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
