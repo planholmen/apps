261726cfaf5b672246eb288b3c5c2ede47a0c21a
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-01-07 16:36:06 +0100

    Fix title on user info page

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -9,7 +9,7 @@
         <div class="sm:w-full lg:w-1/2 mx-auto text-center sm:text-6xl lg:text-4xl py-12">
 
             <div>
-                <a href="/">Opdater indstillinger</a>
+                <a href="/">Opdater oplysninger</a>
             </div>
 
             <form action="/user/me/update" method="post" enctype="multipart/form-data">
