e1dff341f5b9d6eca5f56f0f3cc01cfbd4e98ee4
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-01-15 18:30:28 +0100

    (#5) Initial work for uploading expenses through jobs

---
+++ app/Jobs/UploadExpense.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Expense;
+use App\Http\Controllers\GoogleDriveController;
+use Google_Service_Drive_DriveFile;
+use http\Exception;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class UploadExpense implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $expense;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param Expense $expense
+     */
+    public function __construct(Expense $expense)
+    {
+        $this->expense = $expense;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws FileNotFoundException
+     */
+    public function handle()
+    {
+        $file = 'public/' . $this->expense->file_path;
+        $name = $this->expense->ph_id . " - " . $this->expense->department . " " . $this->expense->activity . " - " . $this->expense->creditor . "." . File::extension($file);
+
+        $metadata = new Google_Service_Drive_DriveFile(array(
+            'name' => $name,
+            'parents' => explode(",", env('DRIVE_EXPENSE_PARENT'))
+        ));
+
+        $content = Storage::get($file);
+
+        GoogleDriveController::createFile($metadata, array(
+            'data' => $content,
+            'mimeType' => File::mimeType(Storage::path($file)),
+            'uploadType' => 'multipart'
+        ));
+
+        $this->expense->uploaded = true;
+        $this->expense->save();
+    }
+
+    /**
+     * The job failed to process.
+     *
+     * @param  Exception  $exception
+     */
+    public function failed(Exception $exception)
+    {
+        // TODO send email to admins with the error message
+
+    }
+}
