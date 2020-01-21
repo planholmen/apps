df254ce4c233cd2e84eb4cbcc6916f8c6d26dcc6
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-01-21 20:00:28 +0100

    Add Danish localization

---
+++ resources/lang/da/validation.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'file' => ':attribute skal være en fil.',
+    'gt' => [
+        'numeric' => ':attribute skal være større end :value.',
+    ],
+    'mimes' => ':attribute skal være en fil af en af typerne: :values.',
+    'min' => [
+        'string' => ':attribute skal have en længde på mindst :min bogstaver.',
+    ],
+    'numeric' => ':attribute skal være et tal.',
+    'required' => ':attribute er påkrævet.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [],
+
+];
