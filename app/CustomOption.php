12289fa1c31b02400a86143ce88f21b736b173e3
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-01-26 21:19:29 +0100

    (#16) Add method to get value by key

--- app/CustomOption.php
+++ app/CustomOption.php
@@ -6,7 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class CustomOption extends Model
 {
+
+    public $primaryKey = 'key';
+
     protected $fillable = [
         'key', 'value'
     ];
+
+
+    /**
+     * Get value for specified custom option key
+     *
+     * @param string $key
+     * @return mixed
+     */
+    public static function get(string $key)
+    {
+        if (CustomOption::find($key) != null)
+            return CustomOption::find($key)->value;
+
+
+        return false;
+    }
 }
