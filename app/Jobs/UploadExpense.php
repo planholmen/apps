a9804553006c1f1eb2b4bc6b3517dcba1311d934
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-01-28 22:58:16 +0100

    (#17) Add ph_id on top of image files

--- app/Jobs/UploadExpense.php
+++ app/Jobs/UploadExpense.php
@@ -2,13 +2,14 @@
 
 namespace App\Jobs;
 
+use Ajaxray\PHPWatermark\Watermark;
 use App\CustomOption;
 use App\Expense;
 use App\Http\Controllers\GoogleDriveController;
 use App\Mail\FailedExpenseUploadJob;
 use App\User;
-use Google_Service_Drive_DriveFile;
 use Exception;
+use Google_Service_Drive_DriveFile;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -18,6 +19,8 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
+use NMC\ImageWithText\Image;
+use NMC\ImageWithText\Text;
 
 class UploadExpense implements ShouldQueue
 {
@@ -51,16 +54,41 @@ class UploadExpense implements ShouldQueue
             'parents' => explode(",", CustomOption::get('DRIVE_EXPENSE_PARENT'))
         ));
 
-        $content = Storage::get($file);
+        $extensions = ['jpeg', 'jpg', 'png', 'gif', 'bmp'];
+        if (in_array(File::extension(Storage::path($file)), $extensions, false)) {
+            $this->addTextToImage($file);
+        }
+
+        if (File::extension(Storage::get($file)) == 'pdf') {
+            $this->addTexToPdf($file);
+        }
 
         GoogleDriveController::createFile($metadata, array(
-            'data' => $content,
+            'data' => Storage::get($file),
             'mimeType' => File::mimeType(Storage::path($file)),
             'uploadType' => 'multipart'
         ));
 
         $this->expense->uploaded = true;
         $this->expense->save();
+    }
+
+    private function addTextToImage($file)
+    {
+        $text = new Text('Bilagsnr.: ' . $this->expense->ph_id);
+        $text->size = 20;
+        $text->font = '/usr/share/fonts/truetype/dejavu/DejaVuSerif.ttf';
+        $text->startX = 5;
+        $text->startY = 5;
+
+        $image = new Image(Storage::path($file));
+        $image->addText($text);
+        $image->render(Storage::path($file));
+    }
+
+    private function addTexToPdf($file)
+    {
+        // TODO Add image to pdf files
     }
 
     /**
