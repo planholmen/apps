c87dd00d096e34f05d1faf58b6368bd0c2172f57
Gustav Utke Kauman <gustav.kauman@gmail.com>
2020-07-03 21:11:18 +0200

    Add comments

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,17 @@ Route::get('/auth/login', 'LoginController@login');
 Route::middleware(['auth'])->group(function () {
     Route::get('/auth/logout', 'LoginController@logout')->name('logout');
 
+    // Expenses
     Route::get('/expense/create', 'ExpenseController@create');
     Route::post('/expense/store', 'ExpenseController@store');
 
+    // Drives
     Route::get('/drive', 'DriveController@index');
     Route::get('/drive/create', 'DriveController@create');
     Route::get('/drive/transfer', 'DriveController@transfer');
     Route::post('/drive/store', 'DriveController@store');
 
+    // Users
     Route::get('/user/me', 'UserController@settings');
     Route::post('/user/me/update', 'UserController@update');
 });
